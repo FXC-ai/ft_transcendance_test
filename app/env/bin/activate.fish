02f34527dec2403521ef0a58550bec0e317ca16b
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2024-03-06 17:39:39 +0100

    static files to take in count

--- app/env/bin/activate.fish
+++ app/env/bin/activate.fish
@@ -1,5 +1,5 @@
 # This file must be used with "source <venv>/bin/activate.fish" *from fish*
-# (https://fishshell.com/); you cannot run it directly.
+# (https://fishshell.com/). You cannot run it directly.
 
 function deactivate  -d "Exit virtual environment and return to normal shell environment"
     # reset old environment variables
@@ -33,7 +33,7 @@ end
 # Unset irrelevant variables.
 deactivate nondestructive
 
-set -gx VIRTUAL_ENV "/Users/francois-xavier/Documents/ft_transcendance_/app/env"
+set -gx VIRTUAL_ENV "/Users/fcoindre/Desktop/ft_transcendance_test/app/env"
 
 set -gx _OLD_VIRTUAL_PATH $PATH
 set -gx PATH "$VIRTUAL_ENV/bin" $PATH
