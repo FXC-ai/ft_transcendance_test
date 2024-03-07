a67f1ad7baf7c600ce4607e9075a6c4291082547
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2024-03-07 16:09:32 +0100

    module members added

--- app/ft_t_django/settings.py
+++ app/ft_t_django/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'members',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +128,9 @@ STATIC_ROOT = BASE_DIR / "staticfiles"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR / "mediafiles"
+
+CSRF_TRUSTED_ORIGINS = ["http://localhost:7890"]
